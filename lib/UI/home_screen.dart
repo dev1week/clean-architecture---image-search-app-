2f81241d356d804b6c41d9a40668d594b917178a
dev1week <rondo2860@gmail.com>
2025-12-27 12:10:25 +0900

    feat : add a search bar

---
+++ lib/UI/home_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("이미지 검색 앱", style: TextStyle(color: Colors.black)),
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                ),
+                suffixIcon: IconButton(
+                  onPressed: () {
+
+                  },
+                  icon: const Icon(Icons.search),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

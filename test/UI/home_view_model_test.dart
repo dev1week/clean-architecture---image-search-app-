91eec6e83ca80f1f1b31fda91e2f01164c0071c3
dev1week <rondo2860@gmail.com>
2026-01-01 17:14:32 +0900

    inject a mock repository

--- test/UI/home_view_model_test.dart
+++ test/UI/home_view_model_test.dart
@@ -1,12 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:image_search/UI/home_view_model.dart';
 import 'package:image_search/data/photo_api_repository.dart';
-import 'package:image_search/data/pixabay_api.dart';
 import 'package:image_search/model/Photo.dart';
 
 void main() {
   test('when api fetch is invoked then photo stream updated ', () async {
-    final viewModel = HomeViewModel(PixabayApi());
+    final viewModel = HomeViewModel(FakePhotoApiRepository());
 
     await viewModel.fetch(('apple'));
 

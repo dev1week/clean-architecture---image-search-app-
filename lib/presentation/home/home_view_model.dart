11b0bef37e90f80dc155b686f10fb801ebf458d6
dev1week <rondo2860@gmail.com>
2026-01-17 18:01:45 +0900

    Refactor: Combine photos and loading state into a single HomeState

--- lib/presentation/home/home_view_model.dart
+++ lib/presentation/home/home_view_model.dart
@@ -1,36 +1,37 @@
 import 'dart:async';
 import '../../data/data_source/result.dart';
 import '../../domain/repository/photo_api_repository.dart';
-import '../../domain/model/photo.dart';
+import 'home_state.dart';
 import 'home_ui_event.dart';
 
 class HomeViewModel {
   final PhotoApiRepository repository;
 
-  final _photoStreamController = StreamController<List<Photo>>()..add([]);
-  Stream<List<Photo>> get photoStream => _photoStreamController.stream;
+  HomeViewModel(this.repository);
 
   final _eventController = StreamController<HomeUiEvent>();
   Stream<HomeUiEvent> get eventStream => _eventController.stream;
 
-  final _loadingController = StreamController<bool>()..add(false);
-  Stream<bool> get isLoadingStream => _loadingController.stream;
+  final _stateController = StreamController<HomeState>()..add(HomeState());
+  Stream<HomeState> get stateStream => _stateController.stream;
+  HomeState _state = HomeState();
 
+  Future<void> fetch(String query) async {
+    _updateState(_state.copyWith(isLoading: true));
 
-  HomeViewModel(this.repository);
-
-  Future<void> fetch(String query) async{
-    _loadingController.add(true);
-
-    final Result<List<Photo>> result = await repository.fetch(query);
+    final result = await repository.fetch(query);
 
     switch (result) {
-      case Success<List<Photo>>():
-        _photoStreamController.add(result.data);
-      case Error<List<Photo>>():
-        _eventController.add(HomeUiEvent.showSnackBar(result.message));
+      case Success(data: final data):
+        _updateState(_state.copyWith(photos: data, isLoading: false));
+      case Error(message: final msg):
+        _updateState(_state.copyWith(isLoading: false));
+        _eventController.add(HomeUiEvent.showSnackBar(msg));
     }
+  }
 
-    _loadingController.add(false);
+  void _updateState(HomeState newState) {
+    _state = newState;
+    _stateController.add(_state);
   }
 }

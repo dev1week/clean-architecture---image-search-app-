d26170b58ae747281d3c2c816ed742fc2714784b
dev1week <rondo2860@gmail.com>
2026-01-17 14:28:13 +0900

    use a dependency injected instance

--- lib/data/data_source/pixabay_api.dart
+++ lib/data/data_source/pixabay_api.dart
@@ -16,7 +16,7 @@ class PixabayApi{
   final key = '53890814-cfa8ab286b708e4201f7aeb5c';
 
   Future<Iterable<dynamic>> fetch(String query) async{
-    final response =  await http.get(Uri.parse('$baseUrl?key=$key&q=$query&image_type=photo&pretty=true'));
+    final response =  await client.get(Uri.parse('$baseUrl?key=$key&q=$query&image_type=photo&pretty=true'));
 
     Map<String, dynamic> jsonResponse = jsonDecode(response.body);
     Iterable hits = jsonResponse['hits'];
